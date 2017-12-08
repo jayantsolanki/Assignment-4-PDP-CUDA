7c3d61f6e9c355cc82d28a5df2acddb970d4b99e
Jayant <jayantjnp@gmail.com>
2017-12-07 23:41:50 -0500

    hello world

---
+++ A3/ex01.cpp
@@ -0,0 +1,19 @@
+// ex01.cpp
+#include <vector>
+#include <cuda_runtime_api.h>
+int main(int argc, char* argv[]) {
+int n = 4 * 1024 * 1024;
+int size = n * sizeof(float);
+std::vector<float> x(n);
+std::vector<float> y(n);
+float* d_x;
+float* d_y;
+cudaMalloc(&d_x, size);
+cudaMalloc(&d_y, size);
+cudaMemcpy(d_x, x.data(), size, cudaMemcpyHostToDevice);
+cudaMemcpy(d_y, y.data(), size, cudaMemcpyHostToDevice);
+// ...
+cudaMemcpy(d_y, y.data(), size, cudaMemcpyDeviceToHost);
+cudaFree(d_x);
+return 0;
+}
